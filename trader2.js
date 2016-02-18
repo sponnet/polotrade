f7c98a3caf4dcb38c43c7e4fc09d8a6347c592bf
Stefaan Ponnet <stefaan@ponnet.com>
2016-02-18 20:00:57 +0100

    trader2.js

--- trader2.js
+++ trader2.js
@@ -1,6 +1,16 @@
 var Poloniex = require('poloniex.js');
 var poloniex = new Poloniex('API_KEY', 'API_SECRET');
+/*
 poloniex.getTicker(function(err, data){
+    if (err){
+        // handle error
+    }
+
+    console.log(data);
+});
+*/
+
+poloniex.getOrderBook('BTC', 'ETH', function(err, data){
     if (err){
         // handle error
     }
